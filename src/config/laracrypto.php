bca8c5a250eb38c5a96103ba3f84b45402250b78
pxgamer <owzie123@gmail.com>
2017-11-21 12:25:13 +0000

    Added Ethereum config values to config/laracrypto

--- src/config/laracrypto.php
+++ src/config/laracrypto.php
@@ -1,9 +1,13 @@
 <?php
 return [
     'blockchain' => [
-        'guid'    => env('BLOCKCHAIN_GUID'),
-        'api'     => env('BLOCKCHAIN_API'),
-        'pass'    => env('BLOCKCHAIN_PASS'),
-        'pass2'   => env('BLOCKCHAIN_PASS2'),
+        'guid'  => env('BLOCKCHAIN_GUID'),
+        'api'   => env('BLOCKCHAIN_API'),
+        'pass'  => env('BLOCKCHAIN_PASS'),
+        'pass2' => env('BLOCKCHAIN_PASS2'),
+    ],
+    'ethereum'   => [
+        'url' => env('ETHEREUM_URL', 'http://localhost'),
+        'port'  => env('ETHEREUM_PORT', 8545),
     ],
 ];
