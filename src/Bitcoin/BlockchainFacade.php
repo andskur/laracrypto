3dcd767be437b7f5f50b88d2e3a668f205c31fb0
pxgamer <owzie123@gmail.com>
2017-12-07 12:56:31 +0000

    PSR-2 style fixes for the Bitcoin classes

--- src/Bitcoin/BlockchainFacade.php
+++ src/Bitcoin/BlockchainFacade.php
@@ -4,8 +4,14 @@ namespace Andskur\Laracrypto\Bitcoin;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class BlockchainFacade.
+ */
 class BlockchainFacade extends Facade
 {
+    /**
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'blockchain';
